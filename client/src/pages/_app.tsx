f15fe47affc55c7c7123fcb58ebc613de57938d3
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2021-02-09 18:30:32 -0500

    navbar component and index page adjusted

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,24 @@
-import "../styles/globals.css";
+import "../styles/tailwind.css";
 import { AppProps } from "next/app";
 import Axios from "axios";
+import { useRouter } from "next/router";
+
+import Navbar from "../components/Navbar";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
 Axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
+  return (
+    <>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
