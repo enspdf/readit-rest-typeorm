9473e30a5181a04a6f09f3e49752b12f95593851
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2021-02-08 19:23:03 -0500

    Create Nextjs client app, install and configure tailwindcss and create register page

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      spacing: {
+        70: "17.5rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
