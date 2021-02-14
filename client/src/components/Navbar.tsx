cbf1a7776499bade91821e963a0dfa866bbb2d05
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2021-02-14 13:02:52 -0500

    Adjust the Navbar to allow the users to search and autocomplete the sub results... algo the submit page redirect user if not logged in using server side validation

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import RedditLogo from "../images/reddit.svg";
 import { useAuthDispatch, useAuthState } from "../context/auth";
 import Axios from "axios";
+import { Sub } from "../types";
+import { useRouter } from "next/router";
 
 const Navbar: FC = ({}) => {
+  const [name, setName] = useState("");
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+  const router = useRouter();
 
   const logout = () => {
     Axios.get("/auth/logout")
@@ -16,6 +23,35 @@ const Navbar: FC = ({}) => {
         window.location.reload();
       })
       .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (name.trim() === "") {
+      setSubs([]);
+      return;
+    }
+
+    searchSubs();
+  }, [name]);
+
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await Axios.get(`/subs/search/${name}`);
+          setSubs(data);
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 250)
+    );
+  };
+
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName("");
   };
 
   return (
@@ -30,13 +66,38 @@ const Navbar: FC = ({}) => {
           <Link href="/">readit</Link>
         </span>
       </div>
-      <div className="flex items-center mx-auto bg-gray-100 border rounded item-center hover:border-blue-500 hover:bg-white">
+      <div className="relative flex items-center mx-auto bg-gray-100 border rounded item-center hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
           type="text"
           className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
           placeholder="Search"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className="absolute left-0 right-0 bg-white"
+          style={{ top: "100%" }}
+        >
+          {subs?.map((sub) => (
+            <div
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+              onClick={() => goToSub(sub.name)}
+            >
+              <Image
+                src={sub.imageUrl}
+                className="rounded-full"
+                alt="Sub"
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+              />
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex">
         {!loading && authenticated ? (
--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import Sidebar from "../../../components/Sidebar";
 import Axios from "axios";
 import { Post } from "../../../types";
+import { GetServerSideProps } from "next";
 
 export default function submit() {
   const [title, setTitle] = useState("");
@@ -81,3 +82,17 @@ export default function submit() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+
+    if (!cookie) throw new Error("Missing auth token cookie");
+
+    await Axios.get("/auth/me", { headers: { cookie } });
+
+    return { props: {} };
+  } catch (error) {
+    res.writeHead(307, { Location: "/login" }).end();
+  }
+};
