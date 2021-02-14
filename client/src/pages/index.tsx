1c33be33abcd7417194ac454a733d45388c9b15b
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2021-02-14 14:24:25 -0500

    Response and ability to create new subs

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { Sub } from "../types";
+import { Post, Sub } from "../types";
 import PostCard from "../components/PostCard";
 import useSWR from "swr";
+import { useAuthState } from "../context/auth";
 // import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { data: posts } = useSWR("/posts");
-  const { data: topSubs } = useSWR("/misc/top-subs");
+  const { data: posts } = useSWR<Post[]>("/posts");
+  const { data: topSubs } = useSWR<Sub[]>("/misc/top-subs");
+  const { authenticated } = useAuthState();
 
   return (
     <>
@@ -16,12 +18,12 @@ export default function Home() {
         <title>readit: the front page of the internet</title>
       </Head>
       <div className="container flex pt-4">
-        <div className="w-160">
+        <div className="w-full px-4 md:w-160 md:p-0">
           {posts?.map((post) => (
             <PostCard post={post} key={post.identifier} />
           ))}
         </div>
-        <div className="ml-6 w-80">
+        <div className="hidden ml-6 md:block w-80">
           <div className="bg-white rounded">
             <div className="p-4 border-b-2">
               <p className="text-lg font-semibold text-center">
@@ -29,19 +31,21 @@ export default function Home() {
               </p>
             </div>
             <div>
-              {topSubs?.map((sub: Sub) => (
+              {topSubs?.map((sub) => (
                 <div
                   key={sub.name}
                   className="flex items-center px-4 py-2 text-xs border-b"
                 >
                   <Link href={`/r/${sub.name}`}>
-                    <Image
-                      src={sub.imageUrl}
-                      className="overflow-hidden rounded-full cursor-pointer"
-                      alt="Sub"
-                      width={(6 * 16) / 4}
-                      height={(6 * 16) / 4}
-                    />
+                    <a>
+                      <Image
+                        src={sub.imageUrl}
+                        className="overflow-hidden rounded-full cursor-pointer"
+                        alt="Sub"
+                        width={(6 * 16) / 4}
+                        height={(6 * 16) / 4}
+                      />
+                    </a>
                   </Link>
                   <Link href={`/r/${sub.name}`}>
                     <a className="ml-2 font-bold hover:cursor-pointer">
@@ -52,6 +56,15 @@ export default function Home() {
                 </div>
               ))}
             </div>
+            {authenticated && (
+              <div className="p-4 border-t-2">
+                <Link href="/subs/create">
+                  <a className="w-full px-2 py-1 blue button">
+                    Create Community
+                  </a>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
